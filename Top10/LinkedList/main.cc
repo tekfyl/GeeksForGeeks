cbdc0cddc0e7bb992b29f2e84c7af9f4bfb9bc03
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-07-26 13:04:57 +0530

    AC

---
+++ Top10/LinkedList/main.cc
@@ -0,0 +1,28 @@
+
+/*
+structure of the node of the list is as
+struct node
+{
+    int data;
+    struct node* next;
+};
+*/
+void sortedInsert(struct node** head_ref, int data)
+{
+    // Code here
+    node *temp = new  node;
+    temp->data = data; temp->next = NULL;
+    if(*head_ref == NULL || data <= (*head_ref)->data){
+        temp->next = *head_ref;
+        *head_ref = temp;
+    }
+    else{
+        node *t=*head_ref;
+        while(t->next != NULL && t->next->data < data){
+            t = t->next;
+        }
+        temp->next = t->next;
+        t->next = temp;
+    }
+}
+
